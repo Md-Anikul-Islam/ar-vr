f6952fea3d7f7c7ad65c33f555b9fa65e92227cc
MD ANIKUL ISLAM <anikul.islam01@northsouth.edu>
2024-06-06 17:21:58 +0600

    up

--- resources/views/layout/root.blade.php
+++ resources/views/layout/root.blade.php
@@ -4,7 +4,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/app-9GQts-QQ.css') }}">
+    <script src="{{ asset('build/app-D3Y-ymQv.js') }}"></script>
 	<link href="{{ asset('favicon.ico') }}" rel="icon" />
 	<title>Bangabndhu Vertual Tour (VR)</title>
 </head>
