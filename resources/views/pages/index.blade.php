80b92814b3c8cf6f34d660928bff12ca4f7ecddf
MD ANIKUL ISLAM <anikul.islam01@northsouth.edu>
2024-06-09 18:08:07 +0600

    some update

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -63,7 +63,7 @@
 
 
   	<!-- Dhanmondi 32 Modal -->
-	<div id="dhanmondi" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+	{{-- <div id="dhanmondi" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
 		<div class="relative p-4 w-full max-w-xl max-h-full">
 			<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 				<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
@@ -90,9 +90,9 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</div> --}}
 	{{-- Backer Hostel Modal --}}
-	<div id="backerHostel" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+	{{-- <div id="backerHostel" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
 		<div class="relative p-4 w-full max-w-xl max-h-full">
 			<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 				<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
@@ -119,9 +119,9 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</div> --}}
 	{{-- Chengchhari Village Modal --}}
-	<div id="chengchhariVillage" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+	{{-- <div id="chengchhariVillage" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
 		<div class="relative p-4 w-full max-w-xl max-h-full">
 			<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 				<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
@@ -148,5 +148,5 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</div> --}}
 @endsection
